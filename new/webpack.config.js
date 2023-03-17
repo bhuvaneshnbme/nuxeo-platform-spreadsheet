463e4efb975dcaed7941ba8e37e2797e9322a419
Bhuvanesh Sahadhevan <111379490+bhuvaneshnbme@users.noreply.github.com>
2023-03-17 19:48:43 +0000

    replaced gulp with webpack

---
+++ new/webpack.config.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const path = require('path');
+const del = require('del');
+const merge = require('merge-stream');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './app/app.js',
+  output: {
+    filename: 'app-build.js',
+    path: path.resolve(__dirname, 'spreadsheet-output')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: '> 0.25%, not dead',
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'styles',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        path.join(__dirname, 'app-build.js'),
+        path.join(__dirname, '../../../target/classes/web/nuxeo.war/spreadsheet/**'),
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'app/images/**/*',
+          to: path.join(__dirname, '../../../target/classes/web/nuxeo.war/spreadsheet/images/[name][ext]'),
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'app'),
+    proxy: {
+      '/nuxeo': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+};
